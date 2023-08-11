a376146508dafa44c95b88fa9910b2e204e48f55
Ricardo Delgado <ridel007@gmail.com>
2023-08-10 21:25:17 -0500

    Game implementation

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float waitTime = 1f;
+    float nextShot = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShot)
+        {
+            nextShot = Time.time + waitTime;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
